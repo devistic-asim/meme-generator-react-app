d2941a59dd9b10fe9f049ca70646003ab3d6e99c
null <null>
2023-11-28 18:04:08 +0500

    handle form

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -7,30 +7,59 @@ function Meme() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         setMemeImage(memesArray[randomNumber].url)
-        
     }
-    
+
+    const [formData, setFormData] = React.useState(
+        {
+            topText: "",
+            bottomText: ""
+        });
+
+    function handleChange(event) {
+        const { name, value, type, checked } = event.target
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }))
+    }
+
+    React.useEffect(() => {
+
+        getMemeImage()
+
+    }, [])
+
     return (
         <main>
             <div className="form">
-                <input 
+                <input
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    onChange={handleChange}
+                    value={formData.topText}
+                    name="topText"
                 />
-                <input 
+                <input
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    onChange={handleChange}
+                    value={formData.bottomText}
+                    name="bottomText"
                 />
-                <button 
+                <button
                     className="form--button"
                     onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={memeImage} className="meme--image" />
+            <div className="wrapper">
+                <img src={memeImage} className="meme--image" />
+                <p className="top-text">{formData.topText}</p>
+                <p className="bottom-text">{formData.bottomText}</p>
+            </div>
         </main>
     )
 }
